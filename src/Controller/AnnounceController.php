2751622cd3ec241fd2437db071655539eb7545b4
Vickou94 <victor.noel@outlook.fr>
2023-03-07 14:21:35 +0100

    annoncesshow

--- src/Controller/AnnounceController.php
+++ src/Controller/AnnounceController.php
@@ -4,11 +4,13 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use App\Repository\AnnounceRepository;
-use Symfony\Component\HttpFoundation\Request;
 use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\Announce;
+use App\Form\AnnounceType;
 
 class AnnounceController extends AbstractController
 {
@@ -27,13 +29,49 @@ class AnnounceController extends AbstractController
     {
         //$entityManager = $doctrine->getManager();
         //$announce = $doctrine->getRepository(Announce::class)->find($id);
-        
-        if ($announce === false) {
+        if (!$announce) {
             throw $this->createNotFoundException("L'annonce n'existe pas");
         }
         
         return $this->render('announce/show.html.twig', [
             'announce' => $announce,
+            'id' => $announce->getId(),
         ]);
     }
+
+    #[Route('/announces/create', name: 'announces/create')]
+    public function create(Request $request, ManagerRegistry $doctrine): Response
+    {
+        $this->denyAccessUnlessGranted('IS_AUTHENTICATED');
+         // Création d'une entité "vide"
+         $announce = new Announce();
+        
+         $form = $this->createForm(AnnounceType::class, $announce);
+         $form->handleRequest($request);
+
+         $user = $this->getUser();
+         $announce->setAuthor($user);
+         
+         // Gestion du formulaire
+         if ($form->isSubmitted() && $form->isValid()) {
+             // Récupérer l'entité qui a été modifiée
+             $announce = $form->getData();
+             $announce->setCreatedAt(new \DateTime());
+             $announce->setupdatedAt(new \DateTime());
+             // Insertion de l'entité en base de données
+             $entityManager = $doctrine->getManager();
+             $entityManager->persist($announce);
+             $entityManager->flush();
+             
+             // Redirection
+             return $this->redirectToRoute('announces.index');
+         }
+         
+         // Affichage du formulaire
+         return $this->render('announce/create.html.twig', [
+             'form' => $form,
+             'user' => $user
+         ]);
+    }
+    
 }
---
+++ src/Form/AnnounceType.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Announce;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class AnnounceType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('title', TextType::class, [
+                'label' => 'Titre de l\'annonce'
+            ])
+            ->add('description', TextareaType::class, [
+                'label' => 'Description'
+            ])
+            ->add('price', IntegerType::class, [
+                'label' => 'Prix'
+            ])
+            ->add('city', TextType::class, [
+                'label' => 'Ville'
+            ])
+            ->add('zipcode', IntegerType::class, [
+                'label' => 'Code Postal'
+            ])
+            ->add('submit', SubmitType::class, [
+                'label' => 'Poster l\'annonce'    
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Announce::class,
+        ]);
+    }
+}
