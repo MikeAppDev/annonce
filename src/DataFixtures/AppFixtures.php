ae8e51514f38e27d47bf232d4a87f291f5f1d375
Vickou94 <victor.noel@outlook.fr>
2023-03-07 10:54:47 +0100

    fixtures

---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\DataFixtures;
+
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use App\Entity\Announce;
+use App\Entity\User;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Faker\Factory;
+use Faker\Generator;
+
+class AppFixtures extends Fixture
+{
+
+        // Classe qui permet de générer des données factices
+        protected Generator $faker;
+    
+        public function __construct(protected UserPasswordHasherInterface $hasher)
+        {
+            $this->faker = Factory::create();
+        }
+
+    public function load(ObjectManager $manager): void
+    {
+
+        // Création d'un utilisateur
+        $user = new User();
+        $user->setEmail('admin@symfoblog.dev');
+        $user->setName('Jean');
+        $user->setPassword($this->hasher->hashPassword($user, 'adminadmin'));
+        $user->setcreatedAt(new \DateTime());
+        $manager->persist($user);
+
+        // Création de plusieurs articles avec des données factices
+        for ($i = 0; $i < 40; $i++) {
+            $announce = new Announce();
+            $announce->setTitle($this->faker->words(3, true));
+            $announce->setDescription($this->faker->paragraphs(4, true));
+            $announce->setPrice(50);
+            $announce->setCity('Marseille');
+            $announce->setZipcode(13000);
+            $announce->setcreatedAt(new \DateTime());
+            $announce->setupdatedAt(new \DateTime());
+            $announce->setAuthor($user);
+            $manager->persist($announce);
+        }
+
+        $manager->flush();
+    }
+}
